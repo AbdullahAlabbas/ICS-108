320ff22bc9f5bfd63ff344f2191036c2bffe1d96
Abdullah Alabbas <SEabdullahT@gmail.com>
2023-01-04 22:23:20 +0300

    .

---
+++ RandomFiles/swap.java
@@ -0,0 +1,19 @@
+package RandomFiles;
+
+public class swap
+{
+    public static void main(String[] args) 
+    {
+        String x = "water";
+        String y = "kool-Aid";
+        String temp;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+    }
+    
+}
---
+++ RandomFiles/variables.java
@@ -0,0 +1,12 @@
+package RandomFiles;
+public class variables
+ {
+    public static void main(String[] args) 
+    {
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Abdullah";
+    }
+ }
