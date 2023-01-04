b49650ba7c3d5f892607c450436c29bc2dee0417
Abdullah Alabbas <SEabdullahT@gmail.com>
2023-01-04 21:38:33 +0300

    .

---
+++ RandomFiles/Main.java
@@ -0,0 +1,11 @@
+package RandomFiles;
+
+public class Main
+ {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");  // sout + tab or sysout + ctrl + space
+        System.out.println("Bye");
+        
+    }
+ }
