f6dfaea65e1edbc62f9f9cbcfa37a04315850c89
Abdullah Alabbas <SEabdullahT@gmail.com>
2023-02-28 12:42:17 +0300

    .

--- RandomFiles/221 Labs/Lab12/Student.java
+++ RandomFiles/221 Labs/Lab12/Student.java
@@ -1,20 +1,20 @@
-public abstract class Student {
-    private int ID;
-    private double gpa;
+// public abstract class Student {
+//     private int ID;
+//     private double gpa;
 
-    public Student(int ID, double gpa) {
-        this.ID = ID;
-        this.gpa = gpa;
-    }
+//     public Student(int ID, double gpa) {
+//         this.ID = ID;
+//         this.gpa = gpa;
+//     }
 
-    public abstract String getStatus();
+//     public abstract String getStatus();
 
-    public double getGpa() {
-        return gpa;
-    }
+//     public double getGpa() {
+//         return gpa;
+//     }
 
-    @Override
-    final public String toString() {
-        return String.format("ID: %d, GPA: %.3f, Status: %s", ID, gpa, getStatus());
-    }
-}
+//     @Override
+//     final public String toString() {
+//         return String.format("ID: %d, GPA: %.3f, Status: %s", ID, gpa, getStatus());
+//     }
+// }
