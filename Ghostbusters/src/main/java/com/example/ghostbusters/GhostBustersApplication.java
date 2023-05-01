eb3bb85521e5d4ac650cbf2a8bec5b68b0a1a989
Abdullah Alabbas <SEabdullahT@gmail.com>
2023-05-02 02:45:12 +0300

    .

--- Ghostbusters/src/main/java/com/example/ghostbusters/GhostBustersApplication.java
+++ Ghostbusters/src/main/java/com/example/ghostbusters/GhostBustersApplication.java
@@ -1,6 +1,20 @@
 package com.example.ghostbusters;
 
-import javafx.animation.AnimationTimer; import javafx.application.Application; import javafx.event.ActionEvent; import javafx.event.EventHandler; import javafx.scene.Scene; import javafx.scene.control.Button; import javafx.scene.image.Image; import javafx.scene.image.ImageView; import javafx.scene.layout.*; import javafx.scene.paint.Color; import javafx.scene.text.Font; import javafx.scene.text.Text; import javafx.stage.Stage;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
 public class GhostBustersApplication extends Application {
     private int score = 0;
     private int objectSpeed = 2;
