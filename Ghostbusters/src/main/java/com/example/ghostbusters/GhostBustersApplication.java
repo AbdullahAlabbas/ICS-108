caf63e232e26078fa9ca1273d7a531892e863f0a
Abdullah Alabbas <SEabdullahT@gmail.com>
2023-05-01 07:55:43 +0300

    remove thirdGhost & unnecessary t

--- Ghostbusters/src/main/java/com/example/ghostbusters/GhostBustersApplication.java
+++ Ghostbusters/src/main/java/com/example/ghostbusters/GhostBustersApplication.java
@@ -1,26 +1,8 @@
 package com.example.ghostbusters;
 
-
-import javafx.animation.AnimationTimer;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
-
-
-
+import javafx.animation.AnimationTimer; import javafx.application.Application; import javafx.event.ActionEvent; import javafx.event.EventHandler; import javafx.scene.Scene; import javafx.scene.control.Button; import javafx.scene.image.Image; import javafx.scene.image.ImageView; import javafx.scene.layout.*; import javafx.scene.paint.Color; import javafx.scene.text.Font; import javafx.scene.text.Text; import javafx.stage.Stage;
 public class GhostBustersApplication extends Application {
     private int score = 0;
-    final private int objectValue = 10;
     private int objectSpeed = 2;
     private int objectsDropped = 0;
     final Pane root = new Pane();
@@ -29,11 +11,9 @@ public class GhostBustersApplication extends Application {
 
     final Ghost ghost1 = new Ghost(10,"Ghost.png");
     final Ghost ghost2 = new Ghost(5,"Ghost2.png");
-    final Ghost ghost3 = new Ghost(15,"Ghost3.png");
 
     final ImageView object1 = ghost1.object();
     final ImageView object2 = ghost2.object();
-    final ImageView object3= ghost3.object();
 
     final Text scoreText = new Text("Score: 0");
     final Text gameOverText = new Text("Game Over");
@@ -75,12 +55,6 @@ public class GhostBustersApplication extends Application {
             scoreText.setText("Score: " + score);
             objectSpeed += 1;
         });
-        object3.setOnMouseClicked(event -> {
-            root.getChildren().remove(object3);
-            score += ghost3.getScore();
-            scoreText.setText("Score: " + score);
-            objectSpeed += 1;
-        });
 
         //startButton
         startButton.setOnMouseClicked(mouseEvent -> {
@@ -100,37 +74,27 @@ public class GhostBustersApplication extends Application {
 
         // restartButton
         restartButton.setOnMouseClicked(event1 -> {
-
                     score = 0;
                     objectSpeed = 2;
                     objectsDropped = 0;
                     gameOverText.setVisible(false);
                     restartButton.setVisible(false);
-
                     scoreText.setText("Score: " + score);
-
 
                     object1.setLayoutX(10);
                     object1.setLayoutY(0);
                     object2.setLayoutX(5);
                     object2.setLayoutY(0);
-                    object3.setLayoutX(7.5);
-                    object3.setLayoutY(0);
-
-                    root.getChildren().addAll(object1, object2,object3);
-
+                    root.getChildren().addAll(object1, object2);
 
                 });
+
         restartButton.setPreserveRatio(true);
         restartButton.setFitHeight(80);
         restartButton.setLayoutX(100);
         restartButton.setLayoutY(440);
         restartButton.setVisible(false);
         root.getChildren().add(restartButton);
-
-
-
-
 
 
         //scene
@@ -147,15 +111,12 @@ public class GhostBustersApplication extends Application {
                     restartButton.setVisible(true);
                     root.getChildren().remove(object2);
                     root.getChildren().remove(object1);
-                    root.getChildren().remove(object3);
-
 
                     return;}
                 int random = (int)Math.floor(Math.random() * 2);
 
                     object1.setLayoutY(object1.getLayoutY() + objectSpeed+2);
                     object2.setLayoutY(object2.getLayoutY() + objectSpeed+1);
-                    object3.setLayoutY(object3.getLayoutY() + objectSpeed+0.5);
 
 
                 if (object1.getLayoutY() > 600 ) {
@@ -165,6 +126,7 @@ public class GhostBustersApplication extends Application {
                     objectsDropped++;
                     root.getChildren().add(object1);
                 }
+
                 if(object2.getLayoutY() > 600){
                     double randomX = Math.random()*300;
                     object2.setLayoutX(randomX);
@@ -172,17 +134,8 @@ public class GhostBustersApplication extends Application {
                     objectsDropped++;
                     root.getChildren().add(object2);
                 }
-                if(object3.getLayoutY() > 600){
-                    double randomX = Math.random()*300;
-                    object3.setLayoutX(randomX);
-                    object3.setLayoutY(0);
-                    objectsDropped++;
-                    root.getChildren().add(object3);
-                }
             }
         };
-
-
     }
 
 
