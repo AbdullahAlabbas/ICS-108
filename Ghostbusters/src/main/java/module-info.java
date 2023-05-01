30a638f837aa7a6705ac879a724d1569bd702c4f
Abdullah Alabbas <SEabdullahT@gmail.com>
2023-05-01 11:12:15 +0300

    .

--- Ghostbusters/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ghostbusters {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens Project.Ghostbusters.src.main.java.com.example.ghostbusters to javafx.fxml;
-    exports Project.Ghostbusters.src.main.java.com.example.ghostbusters;
-}
